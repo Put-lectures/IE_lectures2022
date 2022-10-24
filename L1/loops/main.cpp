da5a4b1c132c65dbdd7f1676ec284e0855086e52
Piotr Kaczmarek <piokac@gmail.com>
2022-10-24 15:10:33 +0200

    initaal programs

---
+++ L1/loops/main.cpp
@@ -0,0 +1,92 @@
+﻿#include <cmath>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int tab[5];
+    //    for (int i = 0; i < 5; i++) {
+    //        cin >> tab[i];
+    //    }
+    //    int i = 0;
+    //    while (i < 5) {
+    //        cout << tab[i];
+    //        i++;
+    //    }
+
+    //    char word[100];
+    //    fstream file("/home/piotr/license.txt", ios::in);
+    //    if (file.is_open()) {
+    //        while (!file.eof()) {
+    //            file >> word;
+
+    //            cout << word << endl;
+    //        }
+    //    }
+
+    //    cout << "Are you sure?(y/n)";
+
+    //    char answer;
+    //    do {
+    //        cout << "Are you sure?(y/n)";
+    //        cin >> answer;
+
+    //        if (answer == 'y' || answer == 'Y') {
+    //            cout << "your answer is yes.";
+    //        }
+    //    } while (!(answer == 'y' || answer == 'Y' || answer == 'n' || answer == 'N'));
+
+    //    //    (answer != 'y' && answer!='Y' && answer != 'n' && answer != 'N')
+
+    //    int n;
+    //    cout << "enter the value to verified";
+    //    cin >> n;
+    //    bool is_prime = true;
+    //    for (int i = 2; (i < sqrt(n) && is_prime; i++) {
+    //        if (n % i == 0) {
+    //            is_prime = false;
+
+    //            cout << i << endl;
+    //            //            bresak;
+    //        }
+    //    }
+
+    //Factorial
+    //    int acc = 1;
+    //    for (int i = 1; i < 5; i++) {
+
+    //        acc *= i;
+    //    }
+    //    cout << acc;
+
+    float grades[100];
+    cout << "What is the number of grades?";
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> grades[i];
+    }
+
+    //    for (int i = 0; i < N; i++) {
+    //        cout << grades[i] << endl;
+    //    }
+
+    //average grade
+    float sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += grades[i];
+    }
+    cout << "the average grade is: " << sum / N << endl;
+    //the worst grade
+    float min = grades[0];
+    for (int i = 1; i < N; i++) {
+        if (min > grades[i]) {
+            min = grades[i];
+        }
+    }
+    cout << "The worst grade is: " << min << endl;
+
+    return 0;
+}
---
+++ L1/simple_program/main.cpp
@@ -0,0 +1,75 @@
+﻿#include <cmath>
+#include <cstring>
+#include <iostream>
+using namespace std;
+// the main function, here the program starts
+
+int main()
+{
+    int a;
+    cin >> a;
+    switch (a) {
+    case 1: {
+        int var = 1;
+        cout << var;
+        break;
+    }
+    case 2: {
+        break;
+    }
+    }
+
+    if (a > 3) {
+        double value = 1.0;
+        cout << value;
+    }
+
+    cout << value;
+
+    //    double c = 4, angle = 30;
+    //    double result = c * sin(angle / 180 * M_PI);
+    //    //    cout << weight / ((height / 100.0) * (height / 100.0));
+    //    cout << result;
+    //    cout << pow(2, 0.25);
+    //    int counter = 0;
+    //Step 1
+    //    counter = counter + 1;
+    //    counter++;
+    //    cout << counter << endl;
+
+    //    //step 2
+    //    counter = counter + 1;
+    //    cout << counter;
+
+    //    int a = 3, b = 4;
+    //    float c;
+
+    //    cout << "Enter the sides length ";
+    //    cin >> a >> b;
+    //    float area = a * b / 2.0;
+    //    c = sqrt(a * a + b * b);
+    //    //    std::cout << "hypotenuse of right triangle with legs " << a << " and " << b << " is " << c
+    //    //              << ", the area is" << area << std::endl;
+    //    printf("hypotenuse of right triangle with legs %d and %d is\n %10.2e, the area is\n %10.2e",
+    //           a,
+    //           b,
+    //           c,
+    //           area);
+    char first_name[20], last_name[20];
+    cout << "What is your name?";
+    cin >> first_name >> last_name;
+    //    cin.getline(name, 20);
+    //    cout << "Hello " << name << endl;
+    printf("Hello %s %s\n", first_name, last_name);
+    char full_name[100];
+    //    strcpy(full_name, first_name);
+    //    strcat(full_name, last_name);
+    //    cout << full_name;
+    sprintf(full_name, "%s %s", first_name, last_name);
+    cout << full_name;
+    //    full_name = first_name + last_name;
+    int a = 5, b = 6, c;
+    c = a + static_cast<float>(b);
+
+    return 0;
+}
